0edb818454aff9e10ce5cab102c4f8797775d4f5
avnerfr <avner.fr@gmail.com>
2026-01-30 17:58:43 +0200

    many initial changes to the pages 3

--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -10,29 +10,6 @@ interface FeatureProps {
 const features: FeatureProps[] = [
   {
     icon: <MedalIcon />,
-<<<<<<< HEAD
-    title: "Accessibility",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
-  },
-  {
-    icon: <MapIcon />,
-    title: "Community",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
-  },
-  {
-    icon: <PlaneIcon />,
-    title: "Scalability",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
-  },
-  {
-    icon: <GiftIcon />,
-    title: "Gamification",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum quas provident cum",
-=======
     title: "Ingest Reality",
     description:
       "Ansora continuously absorbs real practitioner content from the wild",
@@ -41,33 +18,27 @@ const features: FeatureProps[] = [
     icon: <MapIcon />,
     title: "Structure Experience",
     description:
-      "We map who's struggling, where, and why",
+      "We map who’s struggling, where, and why",
   },
   {
     icon: <PlaneIcon />,
     title: "What actually hurts",
     description:
-      "Ansora shows you the pains your customers can't stop talking about. What breaks most often. Who it hits hardest. And which problems are just background noise.",
+      "Ansora shows you the pains your customers can’t stop talking about. What breaks most often. Who it hits hardest. And which problems are just background noise.,",
   },
   {
     icon: <GiftIcon />,
     title: "Write like an insider",
     description:
-      "Pick a real pain. Write marketing that makes people say: \"That's my day-to-day.\"",
->>>>>>> 06dca25 (many initial changes to the pages)
+      "Pick a real pain. Write marketing that makes people say: “That’s my day-to-day.”.",
   },
 ];
 
 export const HowItWorks = () => {
   return (
     <section
-<<<<<<< HEAD
       id="howItWorks"
       className="container text-center py-24 sm:py-32"
-=======
-      id="howitworks"
-      className="container text-center py-24 sm:py-32 bg-[hsl(var(--section-bg-3))]"
->>>>>>> 06dca25 (many initial changes to the pages)
     >
       <h2 className="text-3xl md:text-4xl font-bold ">
         How It{" "}
@@ -77,11 +48,7 @@ export const HowItWorks = () => {
         Step-by-Step Guide
       </h2>
       <p className="md:w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground">
-<<<<<<< HEAD
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
-=======
-        Sigal - add text here
->>>>>>> 06dca25 (many initial changes to the pages)
         dolor pariatur sit!
       </p>
 
