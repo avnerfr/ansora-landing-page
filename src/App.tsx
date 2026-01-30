e7e02a4324c2ce5a27ce5d93dc51faef51398335
avnerfr <avner.fr@gmail.com>
2026-01-30 17:49:58 +0200

    many initial changes to the pages 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import { About } from "./components/About";
-import { Cta } from "./components/Cta";
 import { FAQ } from "./components/FAQ";
 import { Features } from "./components/Features";
 import { Footer } from "./components/Footer";
-import { Hero } from "./components/Hero";
 import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/Navbar";
-import { Newsletter } from "./components/Newsletter";
 import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/ScrollToTop";
-import { Services } from "./components/Services";
-import { Sponsors } from "./components/Sponsors";
 import { Team } from "./components/Team";
 import { Testimonials } from "./components/Testimonials";
 import "./App.css";
@@ -18,35 +12,12 @@ import "./App.css";
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Navbar />
-      <Hero />
-      <Sponsors />
-      <About />
-      <HowItWorks />
-      <Features />
-      <Services />
-      <Cta />
-      <Testimonials />
-      <Team />
-      <Pricing />
-      <Newsletter />
-=======
-      {<Navbar /> }
       <Features />
       <Testimonials />
       <HowItWorks />
-      {/* <Hero /> */}
-      {/* <Sponsors /> */}
-      {/* <About /> */}
-
-      {/* <Services /> */}
-      {/* <Cta /> */}
       <Pricing />
       <Team />
-
-      {/* <Newsletter /> */}
->>>>>>> 06dca25 (many initial changes to the pages)
       <FAQ />
       <Footer />
       <ScrollToTop />
